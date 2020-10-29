88b56f389d45942eae3f4b313c085bb5949cdfe1
Lior Bragilevsky <lbragile.masc@gmail.com>
2020-10-29 15:04:56 -0700

    working with react and cookies

--- client/public/background.js
+++ client/public/background.js
@@ -1,21 +1,21 @@
+var target_url = "http://localhost:3000";
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.create(
     {
       windowId: null,
-      url: "http://localhost:3000",
+      url: target_url,
       active: true,
       openerTabId: tab.id,
     },
     (newTab) => {
-      // newTab.group_name = "StackOverflow";
-      // chrome.tabs.executeScript(newTab.id, { file: "./contents.js" });
-
       // wait for tab to load
       chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
         if (changeInfo.status == "complete" && tabId == newTab.id) {
           chrome.tabs.getAllInWindow(null, (tabs) => {
-            chrome.tabs.sendMessage(newTab.id, { tabs }, (response) => {
-              chrome.tabs.remove(response.remove_tabs);
+            chrome.cookies.set({
+              url: target_url,
+              name: "tabs",
+              value: JSON.stringify(tabs),
             });
           });
         }
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <ul id="tab-urls"></ul>;
-}
+import React, { useRef } from "react";
+import Cookies from "js-cookie";
 
-export default App;
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+export default function App() {
+  var tabs = useRef(Cookies.get("tabs"));
+
+  return (
+    <div>
+      {JSON.parse(tabs.current).map((tab) => {
+        return tab.url.includes("http") && !tab.url.includes("localhost") ? (
+          <div className="inline-block" key={Math.random()}>
+            <img
+              src={tab.favIconUrl}
+              width="16"
+              height="16"
+              alt="icon for the url"
+              className="m-2"
+            />
+            <a href={tab.url} target="_blank" rel="noreferrer">
+              {tab.title}
+            </a>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
+}
