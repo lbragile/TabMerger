2f65218a56d67ed504bffb8da7fed4dec0b42b29
Lior Bragilevsky <lbragile.masc@gmail.com>
2021-12-24 23:30:51 -0800

    added prettier and cspell workflow checks

--- src/store/actions/filter.ts
+++ src/store/actions/filter.ts
@@ -3,15 +3,9 @@
 import { FILTER_ACTIONS } from "../reducers/filter";
 import { IGroupItemState } from "../reducers/groups";
 
-const updateFilteredTabs = (payload: chrome.tabs.Tab[][]) => ({
-  type: FILTER_ACTIONS.UPDATE_FILTERED_TABS,
-  payload
-});
+const updateFilteredTabs = (payload: chrome.tabs.Tab[][]) => ({ type: FILTER_ACTIONS.UPDATE_FILTERED_TABS, payload });
 
-const updateFilteredGroups = (payload: IGroupItemState[]) => ({
-  type: FILTER_ACTIONS.UPDATE_FILTERED_GROUPS,
-  payload
-});
+const updateFilteredGroups = (payload: IGroupItemState[]) => ({ type: FILTER_ACTIONS.UPDATE_FILTERED_GROUPS, payload });
 
 export default {
   updateFilteredTabs,
