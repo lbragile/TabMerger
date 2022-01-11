97db745c6113ca171b16e44a21a573664253ed4b
Lior Bragilevsky <lbragile.masc@gmail.com>
2022-01-10 23:16:29 -0800

    ci: improved dangerfile check output

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,14 +3,31 @@ import { markdown, danger, warn, fail } from "danger";
 markdown(`
 ## PR Snapshot
 
+Comparing: ${danger.git.base.slice(0, 8)}...${danger.git.head.slice(0, 8)}
+
 ### Changed Files:
-- ${danger.git.modified_files.join("\n- ")}
+<details>
+    <summary>See Details</summary>
+    <ul>
+      ${danger.git.modified_files.map((item) => `<li>${item}</li>`)}
+    </ul>
+</details>
 
 ### Created Files:
-- ${danger.git.created_files.join("\n- ")}
+<details>
+    <summary>See Details</summary>
+    <ul>
+      ${danger.git.created_files.map((item) => `<li>${item}</li>`)}
+    </ul>
+</details>
 
 ### Deleted Files:
-- ${danger.git.deleted_files.join("\n- ")}
+<details>
+    <summary>See Details</summary>
+    <ul>
+      ${danger.git.deleted_files.map((item) => `<li>${item}</li>`)}
+    </ul>
+</details>
 `);
 
 const linesAdded = danger.github.pr.additions;
@@ -19,23 +36,31 @@ const BIG_PR_THRESHOLD = 1000;
 const LONG_COMMIT_MESSAGE_THRESHOLD = 125;
 
 const packageChanged = danger.git.modified_files.includes("package.json");
-const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.json");
+const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.yaml");
 
-const malformedCommits = danger.git.commits.filter(
-  (commit) =>
-    !commit.message.match(/^(feat|fix|build|chore|ci|style|refactor|perf|test|docs):/i) ||
-    commit.message.length > LONG_COMMIT_MESSAGE_THRESHOLD
-);
+const malformedCommits = danger.git.commits.filter((commit) => {
+  const subject = commit.message.split("\n")[0];
 
-const PACKAGE_CHECK_MESSAGE = `Changes were made to package.json, but not to pnpm-lock.json - <i>'Perhaps you need to run \`pnpm i\`?'</i>`;
+  return (
+    (!subject.match(/^(feat|fix|build|chore|ci|style|refactor|perf|test|docs):/i) &&
+      !subject.includes("Merge pull request")) ||
+    subject.length > LONG_COMMIT_MESSAGE_THRESHOLD
+  );
+});
 
-const BIG_PR_MESSAGE = `Big PR (#added: ${linesAdded}, #deleted: ${linesDeleted}), please keep your PR small to make it easier to review`;
+const PACKAGE_CHECK_MESSAGE = `Changes found in package.json, but not in pnpm-lock.yaml - <i>'Forgot to run \`pnpm i\`?'</i>`;
+
+const BIG_PR_MESSAGE = `Big PR (# added: <b>${linesAdded}</b>, # deleted: <b>${linesDeleted}</b>), please keep your PR small to make it easier to review`;
 
 const MALFORMED_COMMIT_MESSAGE = `
 Some commit messages do not match the expected format (helps us generate changelogs).\n\n
-Violating commits:\n
-- ${malformedCommits.map((item) => `${item.message} (\`${item.sha}\`)`).join("\n- ")}\n\n
-See <a href='https://www.conventionalcommits.org/en/v1.0.0/'>Conventional Commits</a> for proper formatting.
+<details>
+    <summary>Violating Commits</summary>
+    <ul>
+      ${malformedCommits.map((item) => `<li>${item.message.split("\n")[0]} (\`${item.sha}\`)</li>`)}
+    </ul>
+</details>\n
+See <a href='https://www.conventionalcommits.org/en/v1.0.0/'>Conventional Commits</a> for expected formatting.
 `;
 
 if (packageChanged && !lockfileChanged) warn(PACKAGE_CHECK_MESSAGE);
