de1767a8d01e0921936837568654b9e488e0ebb4
Lior Bragilevsky <lbragile.masc@gmail.com>
2022-01-11 20:30:14 -0800

    ci: added file size check to danger action & simplified collapsible list genration

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,72 +1,102 @@
 import { markdown, danger, warn, fail } from "danger";
 
-markdown(`
-## PR Snapshot
-
-Comparing: ${danger.git.base.slice(0, 8)}...${danger.git.head.slice(0, 8)}
-
-### Changed Files:
-<details>
-    <summary>See Details</summary>
-    <ul>
-      ${danger.git.modified_files.map((item) => `<li>${item}</li>`).join("\n")}
-    </ul>
-</details>
-
-### Created Files:
-<details>
-    <summary>See Details</summary>
-    <ul>
-      ${danger.git.created_files.map((item) => `<li>${item}</li>`).join("\n")}
-    </ul>
-</details>
-
-### Deleted Files:
-<details>
-    <summary>See Details</summary>
-    <ul>
-      ${danger.git.deleted_files.map((item) => `<li>${item}</li>`).join("\n")}
-    </ul>
-</details>
-`);
-
-const linesAdded = danger.github.pr.additions;
-const linesDeleted = danger.github.pr.deletions;
 const BIG_PR_THRESHOLD = 1000;
 const LONG_COMMIT_MESSAGE_THRESHOLD = 125;
 
-const packageChanged = danger.git.modified_files.includes("package.json");
-const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.yaml");
-
-const malformedCommits = danger.git.commits.filter((commit) => {
-  const subject = commit.message.split("\n")[0];
-
-  return (
-    (!subject.match(/^(feat|fix|build|chore|ci|style|refactor|perf|test|docs):/i) &&
-      !subject.includes("Merge pull request")) ||
-    subject.length > LONG_COMMIT_MESSAGE_THRESHOLD
-  );
-});
-
-const PACKAGE_CHECK_MESSAGE = `Changes found in package.json, but not in pnpm-lock.yaml - <i>'Forgot to run \`pnpm i\`?'</i>`;
-
-const BIG_PR_MESSAGE = `Big PR (# added: <b>${linesAdded}</b>, # deleted: <b>${linesDeleted}</b>), please keep your PR small to make it easier to review`;
-
-const MALFORMED_COMMIT_MESSAGE = `
-Some commit messages do not match the expected format (helps us generate changelogs).
-<br />
-<br />
-<details>
-    <summary>Violating Commits</summary>
+function generateCollapsibleList(items: string[], summary: string): string {
+  return `
+  <details>
+    <summary>${summary}</summary>
     <ul>
-      ${malformedCommits.map((item) => `<li>${item.message.split("\n")[0]} (${item.sha.slice(0, 8)})</li>`).join("\n")}
+      ${items.map((item) => `<li>${item}</li>`).join("\n")}
     </ul>
-</details>
-See <a href='https://www.conventionalcommits.org/en/v1.0.0/'>Conventional Commits</a> for expected formatting.
-`;
+  </details>
+  `;
+}
 
-if (packageChanged && !lockfileChanged) warn(PACKAGE_CHECK_MESSAGE);
+(async function () {
+  const packageChanged = danger.git.modified_files.includes("package.json");
+  const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.yaml");
+  const PACKAGE_CHECK_MESSAGE = `Changes found in package.json, but not in pnpm-lock.yaml - <i>'Forgot to run \`pnpm i\`?'</i>`;
 
-if (linesAdded + linesDeleted > BIG_PR_THRESHOLD) warn(BIG_PR_MESSAGE);
+  const linesAdded = danger.github.pr.additions;
+  const linesDeleted = danger.github.pr.deletions;
+  const BIG_PR_MESSAGE = `Big PR (# added: <b>${linesAdded}</b>, # deleted: <b>${linesDeleted}</b>), please keep your PR small to make it easier to review`;
 
-if (malformedCommits.length > 0) fail(MALFORMED_COMMIT_MESSAGE);
+  const malformedCommits = danger.git.commits.filter((commit) => {
+    const subject = commit.message.split("\n")[0];
+
+    return (
+      (!subject.match(/^(feat|fix|build|chore|ci|style|refactor|perf|test|docs):/i) && !subject.includes("Test")) ||
+      subject.length > LONG_COMMIT_MESSAGE_THRESHOLD
+    );
+  });
+
+  const MALFORMED_COMMIT_MESSAGE = `
+  Some commit messages do not match the expected format (helps us generate changelogs).
+  <br />
+  <br />
+  <details>
+      <summary>Violating Commits</summary>
+      <ul>
+        ${malformedCommits
+          .map((item) => `<li>${item.message.split("\n")[0]} (${item.sha.slice(0, 7)})</li>`)
+          .join("\n")}
+      </ul>
+  </details>
+  See ${danger.utils.href(
+    "https://www.conventionalcommits.org/en/v1.0.0/",
+    "Conventional Commits"
+  )} for expected formatting.
+  `;
+
+  const filesChanged = [...danger.git.modified_files, ...danger.git.created_files, ...danger.git.deleted_files];
+
+  const fileSizeMapper: Record<"filename" | "base" | "current" | "diff" | "percent", string>[] = [];
+  for (const file of filesChanged) {
+    const { before, after } = await danger.git.diffForFile(file);
+
+    const unMinifiedSizeDiffInBytes = after.length - before.length;
+    const percentageChanged = (unMinifiedSizeDiffInBytes * 100) / before.length;
+
+    fileSizeMapper.push({
+      filename: file,
+      base: `${before.length * 1e3} KB`,
+      current: `${after.length * 1e3} KB`,
+      diff: `${unMinifiedSizeDiffInBytes * 1e3}KB`,
+      percent: `${percentageChanged}%`
+    });
+  }
+
+  // MESSAGES
+
+  if (packageChanged && !lockfileChanged) warn(PACKAGE_CHECK_MESSAGE);
+
+  if (linesAdded + linesDeleted > BIG_PR_THRESHOLD && !lockfileChanged) warn(BIG_PR_MESSAGE);
+
+  if (malformedCommits.length > 0) fail(MALFORMED_COMMIT_MESSAGE);
+
+  markdown(`
+  ## PR Snapshot
+  
+  Comparing: ${danger.git.base.slice(0, 7)}...${danger.git.head.slice(0, 7)}
+  
+  ${generateCollapsibleList(danger.git.modified_files, "Changed Files")}
+
+  ${generateCollapsibleList(danger.git.created_files, "Created Files")}
+
+  ${generateCollapsibleList(danger.git.deleted_files, "Deleted Files")}
+  `);
+
+  markdown(`
+  ## File Size Changes
+
+  Comparing: ${danger.git.base.slice(0, 7)}...${danger.git.head.slice(0, 7)}
+
+  | Filename | Base | Current | +/- |  %  |
+  |---------:|:----:|:-------:|:---:|:---:|
+  ${fileSizeMapper
+    .map((item) => `| ${item.filename} | ${item.base} | ${item.current} | ${item.diff} | ${item.percent} |`)
+    .join("\n")}
+  `);
+})();
